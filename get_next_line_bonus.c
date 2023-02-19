f708c1296ec6a6e7f65b69edad2030f13ec4a2ff
Yusuf Salih Mamur <yumamur@k1m33s10.42istanbul.com.tr>
2023-02-20 01:09:57 +0300

    destroy silinebilir gibi

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,166 +1,151 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yumamur <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/11 16:46:14 by yumamur           #+#    #+#             */
-/*   Updated: 2023/02/19 13:50:00 by yumamur          ###   ########.fr       */
+/*   Created: 2023/02/11 16:45:49 by yumamur           #+#    #+#             */
+/*   Updated: 2023/02/13 18:50:36 by yumamur          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
-#include <stdio.h>
 
-t_list	*which_file(t_list *head, int fd)
-{
-	t_list	*curr;
-
-	curr = head;
-	if (curr->nbr == ((unsigned int)fd + 1))
-		return (curr);
-	while (curr->nbr)
-	{
-		if (!curr->next)
-		{
-			curr->next = malloc(sizeof(t_list)); 
-			if (!curr->next)
-				return (NULL);
-			curr->next->content = NULL;
-			curr->next->prev = curr;
-			curr->next->nbr = 0;
-			curr->next->next = NULL;
-		}
-		curr = curr->next;
-		if (curr->nbr == ((unsigned int )fd) + 1)
-			return (curr);
-	}
-	curr->nbr = ((unsigned int )fd) + 1;
-	return (curr);
-}
-
-char	*get_to_second_line(char *content)
+static char	*get_to_second_line(char *keep)
 {
 	char	*ret;
 	int		i1;
 	int		i2;
 
 	i1 = 0;
-	while (content[i1] && content[i1] != '\n')
+	while (keep[i1] && keep[i1] != '\n')
 		i1++;
-	if (!content[i1] || (content[i1] == '\n' && content[i1 + 1] == '\0'))
+	if (!keep[i1])
 	{
-		free(content);
+		free(keep);
 		return (NULL);
 	}
-	ret = malloc(ft_strlen(content) - i1);
+	ret = malloc(ft_strlen(keep) - i1);
 	if (!ret)
-	{
-		free(content);
 		return (NULL);
-	}
 	i2 = 0;
-	while (content[++i1])
-		ret[i2++] = content[i1];
+	i1++;
+	while (keep[i1])
+		ret[i2++] = keep[i1++];
 	ret[i2] = '\0';
-	free(content);
+	free(keep);
 	return (ret);
 }
 
-char	*get_first_line(const char *content)
+char	*get_first_line(const char *keep)
 {
 	int		i;
-	int		end;
 	char	*ret;
 
 	i = 0;
-	if (!content[i])
+	if (!keep[i])
 		return (NULL);
-	while (content[i] && content[i] != '\n')
+	while (keep[i] && keep[i] != '\n')
 		i++;
-	end = 1;
-	if (content[i] == '\n')
-		end++;
-	ret = malloc(i + end);
+	ret = malloc(i + 2);
 	if (!ret)
 		return (NULL);
 	i = 0;
-	while (content[i] && content[i] != '\n')
+	while (keep[i] && keep[i] != '\n')
 	{
-		ret[i] = content[i];
+		ret[i] = keep[i];
 		i++;
 	}
-	if (content[i] == '\n')
+	if (keep[i] == '\n')
 		ret[i++] = '\n';
 	ret[i] = '\0';
 	return (ret);
 }
 
-void	read_line(t_list *curr)
+char	*read_line(char *keep, int fd)
 {
 	char	*concat;
 	int		bytes_readed;
 
 	concat = malloc(BUFFER_SIZE + 1);
 	if (!concat)
-		return ;
+		return (NULL);
 	bytes_readed = 1;
-	while (bytes_readed != 0 && !ft_strchr(curr->content, '\n'))
+	while (bytes_readed != 0 && !ft_strchr(keep, '\n'))
 	{
-		bytes_readed = read((curr->nbr - 1), concat, BUFFER_SIZE);
+		bytes_readed = read(fd, concat, BUFFER_SIZE);
 		if (bytes_readed == -1)
 		{
+			free(keep);
 			free(concat);
-			free(curr->content);
-			curr->content = NULL;
-			curr->nbr = 0;
-			return ;
+			return (NULL);
 		}
 		concat[bytes_readed] = '\0';
-		curr->content = ft_strjoin(curr->content, concat);
+		keep = ft_strjoin(keep, concat);
 	}
 	free(concat);
+	return (keep);
 }
 
+t_list *find(t_list **head, int fd)
+{
+	t_list *curr;
+
+	if (*head == NULL){
+		*head = malloc(sizeof(t_list));
+		**head = (t_list){.content = NULL, .next = NULL, .fd = fd};
+		return *head;
+	}
+	curr = *head;
+	while (curr->next != NULL)
+		if (curr->fd == fd)
+			return curr;
+		else
+			curr = curr->next;
+	curr->next = malloc(sizeof(t_list));
+	*curr->next= (t_list){.content = NULL, .next = NULL, .fd = fd};
+	return curr->next;
+}
+
+void *destroy_this(t_list **head, t_list *target)
+{
+	t_list	*curr;
+
+	curr = *head;
+	if (*head == target)
+	{
+		*head = (*head)->next;
+		free(target);
+		return (NULL);
+	}
+	while (1)
+	{
+		if (curr->next == target)
+		{
+			curr->next = curr->next->next;
+			free(target);
+			return (NULL);
+		}
+		else
+		curr = curr->next;
+	}
+	return (NULL);
+}
+ t_list *head;
 char	*get_next_line(int fd)
 {
-	char			*ret;
-	static t_list	head;
-	t_list			*curr;
-	int				ctl;
+	char		*ret;
+
+	t_list *curr;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-
-	curr = which_file(&head, fd);
-	read_line(curr);
-	ctl = 0;
-	if (curr->content == NULL)
-	{
-		ctl = 1;
-		if (curr != &head)
-			curr->prev->next = curr->next;
-		if (curr->next && curr != &head)
-			curr->next->prev = curr->prev;
-		if (curr != &head)
-		{
-			free(curr);
-		}
-		return (NULL);
-	}
+	curr = find(&head, fd);
+	curr->content = read_line(curr->content, fd);
+	if (!curr->content)
+		return (destroy_this(&head, curr));
 	ret = get_first_line(curr->content);
 	curr->content = get_to_second_line(curr->content);
-	if (curr->content == NULL && curr != &head && !ctl)
-	{
-		if (curr->next)
-		{
-			curr->prev->next = curr->next;
-			curr->next->prev = curr->prev;
-		}
-		else
-			curr->prev->next = NULL;
-		free(curr);
-	}
 	return (ret);
 }
